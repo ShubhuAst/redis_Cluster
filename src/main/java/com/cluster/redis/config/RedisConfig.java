aa094fbe37b6384499f49c2f554207a77705dae8
shubhu <shubham.asthana@tothenew.com>
2023-08-24 05:57:56 +0000

    Dockerize project

--- src/main/java/com/cluster/redis/config/RedisConfig.java
+++ src/main/java/com/cluster/redis/config/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -15,8 +14,6 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.Arrays;
 
 @Configuration
 public class RedisConfig {
@@ -28,6 +25,7 @@ public class RedisConfig {
     @Value("${redis.jedis.pool.min-idle}")
     private int minIdle;
 
+    private String redis_ip = System.getenv("REDIS_IP_ADD");
 
 
     @Bean
@@ -43,33 +41,34 @@ public class RedisConfig {
 
     @Bean
     public JedisConnectionFactory getJedisConnectionFactory() {
+        System.out.println("Ip of Redis container:" + redis_ip);
         RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30001));
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30002));
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30003));
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30004));
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30005));
-        clusterConfig.addClusterNode(new RedisNode("127.0.0.1", 30006));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30001));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30002));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30003));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30004));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30005));
+        clusterConfig.addClusterNode(new RedisNode(redis_ip, 30006));
 
         return new JedisConnectionFactory(clusterConfig, getJedisClientConfiguration());
     }
 
     @Bean
-    public RedisTemplate<String,Object> redisTemplate() {
-        RedisTemplate<String,Object> template = new RedisTemplate<>();
+    @Qualifier("cluster")
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(getJedisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new JdkSerializationRedisSerializer());
         template.setHashValueSerializer(new JdkSerializationRedisSerializer());
-        template.setEnableTransactionSupport(true);
         template.afterPropertiesSet();
         return template;
     }
 
     @Bean
     @Qualifier("hashOperations")
-    public HashOperations<String, String, User> getRedisTemplateForHash(){
+    public HashOperations<String, String, User> getRedisTemplateForHash() {
         return redisTemplate().opsForHash();
     }
 }
